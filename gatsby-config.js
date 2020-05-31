9601f3a8cd011d0e2c4e5dfbba0f638440c88c25
kayehol <kaye.holanda@gmail.com>
2020-05-31 20:14:19 -0300

    layout adicionado

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Editora Trajes Solares/Lunares`,
+    description: `Site da editora Trajes Solares/Lunares.`,
+    author: `Kayê Holanda`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,103 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
+import { Link } from "gatsby"
 import "./layout.css"
+import styled from "styled-components"
+import "typeface-poppins"
+import elemAstros from "../images/elemAstros.png"
+import insta from "../images/insta.png"
+import fb from "../images/fb.png"
+import wpp from "../images/wpp.png"
 
+const Header = styled.div`
+  background-color: #fff;
+  display: flex;
+  flex-direction: row;
+  align-items: flex-end;
+  padding-right: 20px;
+  img {
+    width: 110px;
+    max-width: 100  %;
+    padding: 0 0 5px 20px;
+  }
+  #menu {
+    font-family: Poppins, sans-serif;
+    display: flex;
+    justify-content: flex-end;
+    height: 60px;
+    width: 100%;
+  }
+  #menu a {
+    color: #231f20;
+    text-decoration: none;
+    padding: 10px 20px;
+    border-bottom: 4px solid #fff;
+    width: auto;
+  }
+  #menu a:hover {
+    border-bottom: 4px solid #dbc724;
+  }
+`
+const Footer =styled.div`
+  font-family: Poppins, sans-serif;
+  background-color: #758b8b;
+  color: #fff;
+  height: 300px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  #contatos {
+    width: 300px;
+    text-align: left;
+    align-self: center;
+    padding-left: 30px;
+  }
+  #social {
+    text-align: center;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  #social a {
+    padding-right: 40px;
+  }
+`
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+      <Header>
+        <Link to="/">
+          <img src={elemAstros} alt="logo" />
+        </Link>
+        <div id="menu">
+          <Link to="/"><p>SELOS</p></Link>
+          <Link to="/loja/"><p>LOJA</p></Link>
+          <Link to="/autores/"><p>AUTORES</p></Link>
+          <Link to="/blog/"><p>BLOG</p></Link>
+          <Link to="/contato/"><p>CONTATO</p></Link>
+        </div>
+      </Header>
+      {children}
+      <Footer>
+        <div id="contatos">
+          <p>ENTRE EM CONTATO</p>
+          <p>TEL.: (82) 0000-0000</p>
+          <p>E-MAIL: exemplo@gmail.com</p>
+        </div>
+        <div id="social">
+          <a href="#">
+            <img src={insta} alt="instagram" />
+          </a>
+          <a href="#">
+            <img src={fb} alt="facebook" />
+          </a>
+          <a href="#">
+            <img src={wpp} alt="whatsapp" />
+          </a>
+        </div>
+      </Footer>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
